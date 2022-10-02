4d50371675bf5477d7e31b4791e4e51b925b7d51
bg <bagilder@gmail.com>
2022-10-01 21:08:05 -0700

    2d render: continuous movement, boundary clipping, framerate counter

--- trinkets/raycaster/2d_render/2d_render.ino
+++ trinkets/raycaster/2d_render/2d_render.ino
@@ -15,17 +15,29 @@
 #define SCREEN_ROTATION 0 //0=regular landscape, 1=box turn CC 90* portrait, 2=upside down, 3=box turn CW 90* portrait
 
 const byte FOVhalf = 35;  //half of the player's total field of view in degrees, to save a single devision lol will be heading+this and heading-this (edge wrapped)
-const byte maxVel = 2;  //full run speed. we're just guessing to start
+const float maxVel = .25;  //full run speed. we're just guessing to start
 const float turnSpeed = PI/32;  //degrees. how far we tick in a direction on encoder blip. might have to change if converting everything to floats
 const byte numWalls = 5;
+const float maxSpeed = 1;
+const byte deceleration = 2*maxSpeed;
+const byte blockSize = 1;
+//const int rightEdge = (SCREEN_WIDTH-1)/ blockSize;
+const int rightEdge = (SCREEN_WIDTH-1)/ 2;
+const int leftEdge = 0;
+const int bottomEdge = (SCREEN_HEIGHT-1)/ blockSize;
+const int topEdge = 0;
+
+float frameRate;
+int frameRefreshCount = 0;
 
 float pHeading = 0;   //an angle, center of player's vield of view
 float pVelX = 0;
 float pVelY = 0;
 float pSpeed = 0;   //player's movement i guess?
 float pVel = 0;
-float pX = SCREEN_WIDTH/2;
-float pY = SCREEN_HEIGHT/2;
+float pX = rightEdge/2;
+float pY = bottomEdge/2;
+unsigned long tPrevFrame, tCurrentFrame, frameTime;
 
 std::vector<int> wallVectorX;
 std::vector<int> wallVectorY;
@@ -44,7 +56,7 @@ void setup()
 */
   display.print("raycasting demo on the game box\n\nbgilder 23 sept 2022\n\n\nwhy on gods green earth did i decide to\nbuild this from scratch");
   display.display();
-
+  
 
   while(digitalRead(buttPin))  
   {  delay(1);   //take it easy, bud
@@ -63,13 +75,14 @@ void loop()
 {
   random_walls();
   delay(1000);
+  tCurrentFrame = millis();
   while(digitalRead(buttPin))  
   {  delay(10);   //take it easy, bud
       player_movement();
         render_frame();
   }
-  pX = SCREEN_WIDTH/2;
-  pY = SCREEN_HEIGHT/2;
+  pX = rightEdge/2;
+  pY = bottomEdge/2;
   
 
 }
@@ -86,8 +99,8 @@ void random_walls()
   
   for(byte i = 0; i < 2*numWalls; i++)    //build some random walls
   {
-    wallVectorX.push_back(random(SCREEN_WIDTH));
-    wallVectorY.push_back(random(SCREEN_HEIGHT));
+    wallVectorX.push_back(random(rightEdge));
+    wallVectorY.push_back(random(bottomEdge));
   }
 }
 
@@ -98,13 +111,34 @@ void cast_rays()
 
 void render_frame()
 {
-  display.clearDisplay();
+
+  frame_rate();
+
+  display.drawFastVLine(rightEdge, topEdge, bottomEdge, GRAY_WHITE);
+  
+	tCurrentFrame = millis();
+	frameTime = tCurrentFrame - tPrevFrame;
   for(int i = 0; i < 2*numWalls; i+=2)    //start point, end point, start point, end point, color (i think??)
   { display.drawLine(wallVectorX[i],wallVectorY[i],wallVectorX[i+1],wallVectorY[i+1], GRAY_WHITE);
   }
   display.drawLine(pX,pY,pX+5*cos(pHeading),pY+5*sin(pHeading), GRAY_3);
   display.drawPixel(pX,pY, GRAY_WHITE);
   display.display();
+  display.clearDisplay();
+  tPrevFrame = tCurrentFrame;
+}
+
+void frame_rate()
+{
+ 
+  if(++frameRefreshCount > 5)
+  {
+    frameRate = 1000/frameTime;
+    frameRefreshCount = 0;
+  }
+  display.setTextSize(0);
+  display.setCursor(0,0);
+  display.print(frameRate);
 }
 
 void player_movement()
@@ -115,12 +149,12 @@ void player_movement()
   check_encoder();  //update rotational encoder flags
   #endif
 
-  if(flag.encButtFlag)
+  if(!digitalRead(encButtPin))
   {
-    if(++pSpeed > 5)    //will this sneaky auto increment my variable? i think maybe
-    { pSpeed = 5;
+    if(++pSpeed > maxSpeed)    //will this sneaky auto increment my variable? i think maybe
+    { pSpeed = maxSpeed;
     }
-    pVel += pSpeed;
+    pVel += pSpeed / frameTime / blockSize;	//will this give me framerate compensation? framerate = 1/frame time, so.....
     if(pVel > maxVel)
     {  pVel = maxVel;
     }
@@ -130,6 +164,10 @@ void player_movement()
     pX += pVelX;    //these two can be +='d above to save ops but this is for debugging help 
     pY += pVelY;
     flag.encButtFlag = 0;
+  }
+  else
+  {
+  	pSpeed -= deceleration / frameTime;	// tune decel for comfort
   }
   if(flag.CWflag)
   {
@@ -145,4 +183,13 @@ void player_movement()
       pHeading = PI*2;
     flag.CCflag = 0;
   }
+  if(pX > rightEdge -1)
+    pX = rightEdge -1;
+  else if (pX <= leftEdge)
+    pX = leftEdge+1;
+  if(pY > bottomEdge -1)
+    pY = bottomEdge -1;
+  else if(pY <= topEdge)
+    pY = topEdge+1;
+  
 }
