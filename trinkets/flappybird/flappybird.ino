8cb40782c3549ccbe33da52d92a96c9df7327112
bg <bagilder@gmail.com>
2026-07-10 15:53:48 -0400

    flappybird: add fxns for game state, animation, game over, pipe add, bird fly. most are still incomplete

--- trinkets/flappybird/flappybird.ino
+++ trinkets/flappybird/flappybird.ino
@@ -4,6 +4,7 @@
 // 2026-07-09
 ////////////////////
 
+// add bottom pipes to the sprite list first so i can use if each%1 to filter heights
 
 #include <gamebox.h>
 #include <vector>
@@ -25,6 +26,15 @@ struct sObject      //one lone coder sprite implementation
   int spriteWidth;
 };
 
+byte flapSpriteCounter = 0;
+unsigned long tFlapCounter = 0;
+unsigned long tFlapNow = 0;
+int flapCountInterval = 100; //ms, how long each flap sprite is on screen before switching to the next
+int pipeGap = SCREEN_HEIGHT/4;	//will be how far apart the pipes are. might change with difficulty. should probably have a lower bound for playability
+int pipeVx = 1;
+int pipeSpeed = 1; 
+int birdFlyDist = 5;	//this will be a gamefeel thing
+
 /*sObject bird={
   SCREEN_WIDTH/3,
   SCREEN_HEIGHT/3,
@@ -42,6 +52,7 @@ std::vector<sObject> objectVector =       //bird in slot 0, pipes thereafter
   {16.5, 16.5, 0, 0, false, 0, sprite[0]},
   {10.5, 3.5, 0, 0, false, 0, sprite[0]}*/
 };
+
 
 
 void draw_sprites()
@@ -132,5 +143,116 @@ void draw_sprites()
     objectVector[each].y = object2.y;
     objectVector[each].removed = object2.removed;		//still don't remember why we aren't just using the object vector explicitly
   }
+	
 }
 
+animate_sprites()
+{
+  //byte flapSpriteList[4] = {sprite[6], sprite[7], sprite[8], sprite[7]};
+  byte flapSpriteListNumerical[4] = {6,7,8,7};
+  tFlapNow = millis();
+  if((tFlapCounter + flapCountInterval <= tFlapNow) || (tFlapNow - tFlapCounter <= 0))		//should catch underflow
+  {
+  	flapSpriteCounter++%3;	//this is gonna give me an off-by-one i just know it
+  	objectVector[0].image = sprite[flapSpriteListNumerical[flapSpriteCounter]];
+  	tFlapCounter = millis();
+  	pipeVx = (int) tFlapCounter/10000;	//change pipe speed every 10 sec. this isn't the way to do this but i'll fix it later
+  }	
+
+  //bird rotates
+
+
+
+}
+
+
+
+void loop()
+{
+	//check for input
+	#ifdef ENCODERLIBRARY
+  	check_encoder();  //update rotational encoder flags
+  	#endif
+	
+  	if(flag.buttFlag)
+  	{
+  	    bird_fly();
+  	    flag.buttFlag = 0;
+  	}
+
+	//calculate game state
+	for(int each = 1; each < objectVector.size(); each++)
+  	{
+  		bool horizOverlap = 0;	//this is a naive approach. it might feel better gameplaywise if we just have a circle hitbox from the center of the bird sprite (esp with rotations)
+  		if(objectVector[0].x + objectVector[0].spriteWidth/2 >= (objectVector[each].x - objectVector[each].spriteWidth/2))	//right of the left boundary
+  		{	horizOverlap = (objectVector[0].x - objectVector[0].spriteWidth/2 <= (objectVector[each].x + objectVector[each].spriteWidth/2))?1:0;	//and left of the right boundary
+  		}
+  		bool vertOverlap = 0;
+  		if(each%2)	//an odd numbered sprite (bottom pipe)
+  		{
+  			if(objectVector[0].y <= objectVector[each].y)
+  			{	vertOverlap = 1;
+  			}
+  		}
+  		else	//an even numbered sprite (top pipe)
+  		{
+  			if(objectVector[0].y >= objectVector[each].y)
+  			{	vertOverlap = 1;
+  			}
+  		}
+
+  		if(horizOverlap && vertOverlap)
+  		{	game_over();
+  		}
+
+  		objectVector[each].x -= pipeVx;	//move the pipes left
+  	}
+
+	animate_sprites();
+	draw_sprites();
+}
+
+void game_over()
+{
+	while(objectVector[0].x > 0)
+	{
+		;
+		draw_sprites();
+		objectVector[0].removed = true;
+		//and then the garbage collection for removing sprites either here or generically where it happens afterwards
+	}
+	delay(100);
+	//add game over sprite to end of sprite vector
+	//
+	sObject o;
+  	o.x = SCREEN_HEIGHT/2;
+  	o.y = SCREEN_WIDTH/2;
+  	o.image = sprite[11];
+  	o.removed = false;
+  	o.objectType = 1;
+  	objectVector.push_back(o);
+	draw_sprites();
+}
+
+
+void add_pipes()
+{
+	int newPipeHeight = SCREEN_HEIGHT/3; 	//this will be dynamic eventually
+	//randomize height of new pipes
+	//might want to put some bounds eventually so it remains possible to play it as time goes on
+
+
+	sObject newPipe1 = {SCREEN_WIDTH-1, newPipeHeight, 0, 0, 0, false, 1, sprite[9], 10, 10}  //x,y,vx,vy,removed,objectType,image,height,width.
+	objectVector.push_back(newPipe1);	//bottom pipe
+	sObject newPipe2 = {SCREEN_WIDTH-1, newPipeHeight+pipeGap, 0, 0, 0, false, 1, sprite[10], 10, 10}  //x,y,vx,vy,removed,objectType,image,height,width.
+	objectVector.push_back(newPipe2);	//top pipe
+}
+
+
+void bird_fly()
+{
+	//bird goes up
+	objectVector[0].rotationNext = PI/4;
+	objectVector[0].vx += birdFlyDist;
+	objectVector[0].image = sprite[6]; 	//the down-wing flap sprite
+}
